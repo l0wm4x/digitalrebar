19990c7136cdd60433d0a1240fbe8899bb16b330
Peter Bueschel <Peter.Bueschel@citrix.com>
2017-11-08 13:36:43 +0100

    removed libvirt blocking port 53 and updated deploy helper

--- deploy/deploy-locally.sh
+++ deploy/deploy-locally.sh
@@ -5,10 +5,31 @@ if [ -z $1 ]; then
     echo "E.g.:  ./deploy-locally.sh 10.0.0.1/24 -> choose one of: "
     echo "-------------------------------------------------------------------------"
     ip -4 addr
+    exit 1
 fi
-echo "CHECKLIST:"
-echo "----------"
+echo "DO NOT FORGET:"
+echo "--------------"
 echo ""
 echo "Did you updated 'compose/config-dir/api/config/networks/the_admin.json.mac'?"
+echo '{'
+echo '  "category": "admin",'
+echo '  "group": "internal",'
+echo '  "deployment": "system",'
+echo '  "conduit": "dhcp",'
+echo '  "v6prefix": "none",'
+echo '  "ranges": ['
+echo '    {'
+echo '      "name": "dhcp",'
+echo '      "first": "147.75.XXXXXXX/28",'
+echo '      "last": "147.75.XXXXXXXX/28",'
+echo '      "allow_anon_leases": true'
+echo '    }'
+echo '  ],'
+echo '  "router": {'
+echo '    "address": "147.75.XXXXXXXX/28",'
+echo '    "pref": 10'
+echo '  }'
+echo '}'
 read -p "Press a key to continue..."
-. ./run-in-system.sh --access=HOST --admin-ip=$1 --deploy-admin=local --dr_tag=master --con-provisioner --con-dhcp
+
+./run-in-system.sh --access=HOST --admin-ip=$1 --deploy-admin=local --dr_tag=master --con-provisioner --con-dhcp
